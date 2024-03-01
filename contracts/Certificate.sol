b099a15c523093206fa06e11f4d8227da4f48bc9
Jack Dishman <me@jackdishman.com>
2024-03-01 16:26:27 -0700

    updates

---
+++ contracts/Certificate.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Certificate is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function createCertificate(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,45 +1,58 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config({ path: __dirname + "/.env" });
-import "@nomicfoundation/hardhat-toolbox";
 
-const { PK } = process.env;
+const { PK, ETHERSCAN_API_KEY, BASE_API_KEY, POLYGON_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.23",
   networks: {
     // for mainnet
     optimism: {
       url: "https://mainnet.optimism.io",
-      accounts: [],
+      accounts: [PK ?? ``],
+    },
+    'base-mainnet': {
+      url: "https://mainnet.base.org",
+      accounts: [PK ?? ``],
     },
     // for testnet
-    base_goerli: {
-      url: "https://goerli.base.org",
-      accounts: [PK ? PK : ``],
+    'base-sepolia': {
+      url: 'https://sepolia.base.org',
+      accounts: [PK ?? ``],
     },
     "optimism-goerli": {
       url: "https://goerli.optimism.io",
-      accounts: [PK ? PK : ``],
+      accounts: [PK ?? ``],
     },
     "polygon-mumbai": {
       url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [PK ? PK : ``],
+      accounts: [PK ?? ``],
     },
     // for the local dev environment
     "optimism-local": {
       url: "http://localhost:8545",
-      accounts: [],
+      accounts: [PK ?? ``],
     },
   },
   etherscan: {
     apiKey: {
-      mainnet: "YOUR_ETHERSCAN_API_KEY",
+      mainnet: ETHERSCAN_API_KEY ?? ``,
       optimisticEthereum: "YOUR_OPTIMISTIC_ETHERSCAN_API_KEY",
-      arbitrumOne: "YOUR_ARBISCAN_API_KEY",
-      polygonMumbai: "FHXESKTNDHF18AYX8BXXIWVWB4CP5WCW2N",
+      polygonMumbai: POLYGON_API_KEY ?? ``,
+      "base-sepolia": BASE_API_KEY ?? ``,
     },
-  },
+    customChains: [
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+         apiURL: "https://api-sepolia.basescan.org/api",
+         browserURL: "https://sepolia.basescan.org"
+        }
+      }
+    ]
+   },
 };
 
 export default config;
